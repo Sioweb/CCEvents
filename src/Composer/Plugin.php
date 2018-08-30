7d10d6ded885d303057527b2f3b4f05fc7a6f507
Sascha Weidner <support@sioweb.de>
2018-08-30 07:03:32 +0000

    Update Plugin.php

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -11,21 +11,18 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
     }
     
     public static function getSubscribedEvents()
     {
         return array(
             'post-install-cmd' => 'installOrUpdate',
-            'post-update-cmd' => 'installOrUpdate',            
+            'post-update-cmd' => 'installOrUpdate',
         );
-    }    
+    }
     
     public function installOrUpdate($event)
     {
-        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
-        file_put_contents('/tmp/composer.log', get_class($event) . "\n",FILE_APPEND);            
-        file_put_contents('/tmp/composer.log', $event->getName() . "\n",FILE_APPEND);                    
+        echo 'PLUGIN: method: '.__METHOD__.', class: '.get_class($event).', name: '.$event->getName();                   
     }
 }

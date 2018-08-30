7a6b64faf9b0880672eb05f6dbfc85a45f8bc89e
Sascha Weidner <support@sioweb.de>
2018-08-30 06:36:20 +0000

    Add new file

---
+++ src/Plugin/Plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sioweb\CCEvent\Plugin;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+
+class CCEventPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
+    }
+    
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'post-install-cmd' => 'installOrUpdate',
+            'post-update-cmd' => 'installOrUpdate',            
+        );
+    }    
+    
+    public function installOrUpdate($event)
+    {
+        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
+        file_put_contents('/tmp/composer.log', get_class($event) . "\n",FILE_APPEND);            
+        file_put_contents('/tmp/composer.log', $event->getName() . "\n",FILE_APPEND);                    
+    }
+}
